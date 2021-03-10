155a2bc632aab3607f6f6d9399e2c1a3062c014b
ekofebriharsono <ekoaewes2006@gmail.com>
2021-03-10 15:47:59 -0700

    progres 60% Owner

--- php/companyProfile/ownerActions.php
+++ php/companyProfile/ownerActions.php
@@ -2,57 +2,54 @@
 include '../koneksi.php';
 $conn = OpenCon();
 
-if(isset($_POST['submitInsertOwner'])){
-   $owner_name = $_POST['owner_name'];
-   $label_owner = $_POST['label_owner'];
-   $owner_descriptions = $_POST['owner_descriptions'];
-   $instagram = $_POST['instagram'];
-   $facebook = $_POST['facebook'];
-   $whatsapp = $_POST['whatsapp'];
-   
-   $link ="http://localhost/Admin-Soulmate-Art-Course/php/companyProfile/image/owner/";
+$link = "http://localhost/Admin-Soulmate-Art-Course/php/companyProfile/image/owner/";
 
-   $ekstensi_diperbolehkan	= array('png','jpg');
-    $date = date("Y-m-d");
-    $time = date("h-i-sa");
+if (isset($_POST['submitInsertOwner'])) {
+  $owner_name = $_POST['owner_name'];
+  $label_owner = $_POST['label_owner'];
+  $owner_descriptions = $_POST['owner_descriptions'];
+  $instagram = $_POST['instagram'];
+  $facebook = $_POST['facebook'];
+  $whatsapp = $_POST['whatsapp'];
 
-    $nama =$_FILES['image']['name'];
-    $x = explode('.', $nama);
-    $ekstensi = strtolower(end($x));
-    $ukuran	= $_FILES['image']['size'];
-    $file_tmp = $_FILES['image']['tmp_name'];	
+  $ekstensi_diperbolehkan  = array('png', 'jpg');
+  $date = date("Y-m-d");
+  $time = date("h-i-sa");
 
-    $name_db = $date.$time.$nama;
+  $nama = $_FILES['image']['name'];
+  $x = explode('.', $nama);
+  $ekstensi = strtolower(end($x));
+  $ukuran  = $_FILES['image']['size'];
+  $file_tmp = $_FILES['image']['tmp_name'];
 
-    if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){		
-            move_uploaded_file($file_tmp, 'image/owner/'.$name_db);
-            $image = $link.$name_db;
-    }
+  $name_db = $date . $time . $nama;
 
-   $sql = "INSERT INTO `owner` (`id_owner`, `owner_name`, `label_owner`, `owner_descriptions`, `instagram`, `facebook`, `whatsapp`, `image`) 
+  if (in_array($ekstensi, $ekstensi_diperbolehkan) === true) {
+    move_uploaded_file($file_tmp, 'image/owner/' . $name_db);
+    $image = $link . $name_db;
+  }
+
+  $sql = "INSERT INTO `owner` (`id_owner`, `owner_name`, `label_owner`, `owner_descriptions`, `instagram`, `facebook`, `whatsapp`, `image`) 
    VALUES (NULL, '$owner_name', '$label_owner','$owner_descriptions','$instagram','$facebook','$whatsapp','$image')";
-   $res = mysqli_query($conn, $sql);
-   if($res){
-        header('Location: ../../pages/companyProfile/owner.php');
-   } else {
-     header('Location: ../../pages/companyProfile/owner.php?status=gagal');
-   }
-
+  $res = mysqli_query($conn, $sql);
+  if ($res) {
+    header('Location: ../../pages/companyProfile/owner.php?status=201');
+  } else {
+    header('Location: ../../pages/companyProfile/owner.php?status=500');
+  }
 }
 
- if(isset($_POST['submitDeleteOwner'])){
-    $id = $_POST['id'];
- 
-    $sql = "DELETE FROM `owner` WHERE `owner`.`id_owner` = $id";
-    $res = mysqli_query($conn, $sql);
-    if($res){
-        header('Location: ../../pages/companyProfile/owner.php');
-    }else {
-        header('Location: ../../pages/companyProfile/owner.php?status=gagal');
-    }
- 
- }
+if (isset($_POST['submitDeleteOwner'])) {
+  $id = $_POST['id'];
+
+  $sql = "DELETE FROM `owner` WHERE `owner`.`id_owner` = $id";
+  $res = mysqli_query($conn, $sql);
+  if ($res) {
+    header('Location: ../../pages/companyProfile/owner.php?status=202');
+  } else {
+    header('Location: ../../pages/companyProfile/owner.php?status=500');
+  }
+}
 
 
 CloseCon($conn);
-?>
