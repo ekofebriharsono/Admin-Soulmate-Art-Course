3233e4c7294650479fd1e8e81943cb8f506cda98
hafilmashudi <hafilmashudi8@gmail.com>
2021-03-11 21:53:07 +0700

    team actions fixx team o%

--- php/companyProfile/teamActions.php
+++ php/companyProfile/teamActions.php
@@ -2,6 +2,8 @@
 include '../koneksi.php';
 $conn = OpenCon();
 
+$link ="http://localhost/Admin-Soulmate-Art-Course/php/companyProfile/image/team/";
+
 if(isset($_POST['submitInsertTeam'])){
    $team_name = $_POST['team_name'];
    $label_team = $_POST['label_team'];
@@ -10,8 +12,6 @@ if(isset($_POST['submitInsertTeam'])){
    $facebook = $_POST['facebook'];
    $whatsapp = $_POST['whatsapp'];
    
-   $link ="http://localhost/Admin-Soulmate-Art-Course/php/companyProfile/image/team/";
-
    $ekstensi_diperbolehkan	= array('png','jpg');
     $date = date("Y-m-d");
     $time = date("h-i-sa");
@@ -33,9 +33,9 @@ if(isset($_POST['submitInsertTeam'])){
    VALUES (NULL, '$team_name', '$label_team','$team_descriptions','$instagram','$facebook','$whatsapp','$image')";
    $res = mysqli_query($conn, $sql);
    if($res){
-        header('Location: ../../pages/companyProfile/team.php');
+        header('Location: ../../pages/companyProfile/team.php?status=201');
    } else {
-    header('Location: ../../pages/companyProfile/team.php?status=gagal');
+        header('Location: ../../pages/companyProfile/team.php?status=500');
    }
 
 }
@@ -46,13 +46,67 @@ if(isset($_POST['submitInsertTeam'])){
     $sql = "DELETE FROM `team` WHERE `team`.`id_team` = $id";
     $res = mysqli_query($conn, $sql);
     if($res){
-        header('Location: ../../pages/companyProfile/team.php');
+        header('Location: ../../pages/companyProfile/team.php?status=202');
     }else {
-        header('Location: ../../pages/companyProfile/team.php?status=gagal');
+        header('Location: ../../pages/companyProfile/team.php?status=500');
     }
  
  }
+ if (isset($_POST['submitUpdateTeam'])) {
+    $id = $_POST['id'];
+    $team_name = $_POST['team_name'];
+    $label_team = $_POST['label_team'];
+    $team_descriptions = $_POST['team_descriptions'];
+    $instagram = $_POST['instagram'];
+    $facebook = $_POST['facebook'];
+    $whatsapp = $_POST['whatsapp'];
+
+    $ekstensi_diperbolehkan    = array('png', 'jpg');
+    $date = date("Y-m-d");
+    $time = date("h-i-sa");
+
+    $nama = $_FILES['image']['name'];
+    $x = explode('.', $nama);
+    $ekstensi = strtolower(end($x));
+    $ukuran    = $_FILES['image']['size'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+
+    $name_db = $date . $time . $nama;
+
+    if (in_array($ekstensi, $ekstensi_diperbolehkan) === true) {
+        move_uploaded_file($file_tmp, 'image/team/' . $name_db);
+        $image = $link . $name_db;
+    }
+
+    if (@$image != null) {
+        $sql = "UPDATE `client` SET 
+            `client_name` = '$team_name', 
+            `description` = '$label_team', 
+            `link` = '$team_descriptions', 
+            `image` = '$instagram'
+            `image` = '$facebook'
+            `image` = '$whatsapp'
+            `image` = '$image 
+            WHERE `client`.`id_team` =  $id;";
+    } else {
+        $sql = "UPDATE `client` SET 
+         `client_name` = '$team_name', 
+            `description` = '$label_team', 
+            `link` = '$team_descriptions', 
+            `image` = '$instagram'
+            `image` = '$facebook'
+            `image` = '$whatsapp
+        WHERE `client`.`id_team` =  $id;";
+    }
+
+
+    $res = mysqli_query($conn, $sql);
+    if ($res) {
+        header('Location: ../../pages/companyProfile/team.php?status=203');
+    } else {
+        header('Location: ../../pages/companyProfile/team.php?status=500');
+    }
+}
 
 
 CloseCon($conn);
-?>
